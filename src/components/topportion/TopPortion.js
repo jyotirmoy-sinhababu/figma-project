58cc3cab58896df852b05028489f5582e3584c60
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-11-12 08:34:00 +0530

    style added

--- src/components/topportion/TopPortion.js
+++ src/components/topportion/TopPortion.js
@@ -6,26 +6,26 @@ const TopPortion = () => {
     <>
       <div className='firstpart-cnt'>
         <div className='earning-data'>
-          <div>
-            <p>Referral Earning</p>
-            <p>₹2500</p>
+          <div className='first-cnt-data'>
+            <p className='first-cnt-txt'>Referral Earning</p>
+            <p className='first-cnt-num'>₹2500</p>
           </div>
-          <div>
-            <p>Total Referrals</p>
-            <p>7</p>
+          <div className='first-cnt-data'>
+            <p className='first-cnt-txt'>Total Referrals</p>
+            <p className='first-cnt-num'>7</p>
           </div>
-          <div>
-            <p>Wallet Balance</p>
-            <p>₹500</p>
+          <div className='first-cnt-data'>
+            <p className='first-cnt-txt'>Wallet Balance</p>
+            <p className='first-cnt-num'>₹500</p>
           </div>
-          <div>
-            <p>Withdraw Balance</p>
+          <div className='first-cnt-btn'>
+            <button className='balance-withdraw-btn'>Withdraw Balance</button>
           </div>
         </div>
         <div className='ref-code-cnt'>
-          <p>Your Referral Code </p>
+          <p className='ref-code-txt'>Your Referral Code </p>
           <div>
-            <p>E D C H 5 4</p>
+            <p className='ref-code'>E D C H 5 4</p>
           </div>
         </div>
       </div>
